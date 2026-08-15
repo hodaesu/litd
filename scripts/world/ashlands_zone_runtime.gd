8db0c7be34a80c58e228b5d69412356b0dab3df4
hodaesu <aurelienfabre@ymail.com>
2026-08-15 22:03:23 +0200

    feat: expand persistent Ashlands world runtime

--- scripts/world/ashlands_zone_runtime.gd
+++ scripts/world/ashlands_zone_runtime.gd
@@ -4,16 +4,28 @@ class_name AshlandsZoneRuntime
 signal shortcut_unlocked(shortcut_id: String)
 signal zone_discovered(zone_id: String)
 signal campfire_used(zone_id: String)
+signal transition_requested(zone_id: String)
+signal resource_collected(resource_id: String)
+signal encounter_cleared(encounter_id: String)
 
 var discovered_zones: Dictionary = {}
 var unlocked_shortcuts: Dictionary = {}
+var collected_resources: Dictionary = {}
+var cleared_encounters: Dictionary = {}
 var current_zone_id := ""
+var previous_zone_id := ""
+var campfires_used_this_run: Dictionary = {}
 
 func enter_zone(zone_id: String) -> void:
+    previous_zone_id = current_zone_id
     current_zone_id = zone_id
     if not discovered_zones.has(zone_id):
         discovered_zones[zone_id] = true
         zone_discovered.emit(zone_id)
+    ExpeditionManager.on_zone_entered(zone_id)
+
+func request_zone_transition(zone_id: String) -> void:
+    transition_requested.emit(zone_id)
 
 func unlock_shortcut(shortcut_id: String) -> void:
     if shortcut_id == "":
@@ -25,6 +37,27 @@ func unlock_shortcut(shortcut_id: String) -> void:
 func is_shortcut_unlocked(shortcut_id: String) -> bool:
     return bool(unlocked_shortcuts.get(shortcut_id, false))
 
+func is_zone_discovered(zone_id: String) -> bool:
+    return bool(discovered_zones.get(zone_id, false))
+
+func mark_resource_collected(resource_id: String) -> void:
+    if resource_id == "":
+        return
+    collected_resources[resource_id] = true
+    resource_collected.emit(resource_id)
+
+func is_resource_collected(resource_id: String) -> bool:
+    return bool(collected_resources.get(resource_id, false))
+
+func mark_encounter_cleared(encounter_id: String) -> void:
+    if encounter_id == "":
+        return
+    cleared_encounters[encounter_id] = true
+    encounter_cleared.emit(encounter_id)
+
+func is_encounter_cleared(encounter_id: String) -> bool:
+    return bool(cleared_encounters.get(encounter_id, false))
+
 func can_ash_hide(marker_type: String) -> bool:
     return marker_type in ["normal_enemy", "loot", "resource"]
 
@@ -35,16 +68,42 @@ func can_spawn_normal_miniboss(zone_data: Dictionary, alternate_route_available:
     return bool(zone_data.get("miniboss_normal_pool", false)) and alternate_route_available
 
 func use_campfire(zone_id: String) -> void:
+    campfires_used_this_run[zone_id] = true
     campfire_used.emit(zone_id)
+
+func was_campfire_used_this_run(zone_id: String) -> bool:
+    return bool(campfires_used_this_run.get(zone_id, false))
+
+func begin_new_expedition() -> void:
+    campfires_used_this_run.clear()
+    current_zone_id = ""
+    previous_zone_id = ""
+
+func reset_world_progression() -> void:
+    discovered_zones.clear()
+    unlocked_shortcuts.clear()
+    collected_resources.clear()
+    cleared_encounters.clear()
+    campfires_used_this_run.clear()
+    current_zone_id = ""
+    previous_zone_id = ""
 
 func serialize() -> Dictionary:
     return {
         "discovered_zones": discovered_zones,
         "unlocked_shortcuts": unlocked_shortcuts,
-        "current_zone_id": current_zone_id
+        "collected_resources": collected_resources,
+        "cleared_encounters": cleared_encounters,
+        "current_zone_id": current_zone_id,
+        "previous_zone_id": previous_zone_id,
+        "campfires_used_this_run": campfires_used_this_run
     }
 
 func deserialize(data: Dictionary) -> void:
     discovered_zones = data.get("discovered_zones", {}).duplicate(true)
     unlocked_shortcuts = data.get("unlocked_shortcuts", {}).duplicate(true)
+    collected_resources = data.get("collected_resources", {}).duplicate(true)
+    cleared_encounters = data.get("cleared_encounters", {}).duplicate(true)
     current_zone_id = str(data.get("current_zone_id", ""))
+    previous_zone_id = str(data.get("previous_zone_id", ""))
+    campfires_used_this_run = data.get("campfires_used_this_run", {}).duplicate(true)
