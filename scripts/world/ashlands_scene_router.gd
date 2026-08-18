2e910786f536356bc600f3e27712908dc8321a4e
hodaesu <aurelienfabre@ymail.com>
2026-08-18 18:20:14 +0200

    Route Chapter IX veil zones

--- scripts/world/ashlands_scene_router.gd
+++ scripts/world/ashlands_scene_router.gd
@@ -12,6 +12,7 @@ const CHAPTER_05_PATH := "res://scenes/world/chapter_05/"
 const CHAPTER_06_PATH := "res://scenes/world/chapter_06/"
 const CHAPTER_07_PATH := "res://scenes/world/chapter_07/"
 const CHAPTER_08_PATH := "res://scenes/world/chapter_08/"
+const CHAPTER_09_PATH := "res://scenes/world/chapter_09/"
 const DEEP_VESTIGE_PATH := "res://scenes/world/deep_vestiges/"
 const GENERIC_DEEP_VESTIGE_SCENE := DEEP_VESTIGE_PATH + "generic_deep_vestige.tscn"
 const MAIN_SCENE := "res://scenes/Main.tscn"
@@ -25,6 +26,7 @@ var zone_scene_paths := {
     "c06_timeless_garden": CHAPTER_06_PATH + "c06_timeless_garden.tscn", "c06_echo_shore": CHAPTER_06_PATH + "c06_echo_shore.tscn", "c06_mute_doors": CHAPTER_06_PATH + "c06_mute_doors.tscn", "c06_overlap_hamlet": CHAPTER_06_PATH + "c06_overlap_hamlet.tscn", "c06_creature_hollow": CHAPTER_06_PATH + "c06_creature_hollow.tscn", "c06_saen_contact": CHAPTER_06_PATH + "c06_saen_contact.tscn", "c06_walking_boundary": CHAPTER_06_PATH + "c06_walking_boundary.tscn",
     "c07_engineer_refuge": CHAPTER_07_PATH + "c07_engineer_refuge.tscn", "c07_bram_workshop": CHAPTER_07_PATH + "c07_bram_workshop.tscn", "c07_fractured_monastery": CHAPTER_07_PATH + "c07_fractured_monastery.tscn", "c07_veyra_laboratory": CHAPTER_07_PATH + "c07_veyra_laboratory.tscn", "c07_relay_camp": CHAPTER_07_PATH + "c07_relay_camp.tscn", "c07_relay_palace": CHAPTER_07_PATH + "c07_relay_palace.tscn", "c07_edras_aperture": CHAPTER_07_PATH + "c07_edras_aperture.tscn",
     "c08_varkhane_border": CHAPTER_08_PATH + "c08_varkhane_border.tscn", "c08_varkhane_archive": CHAPTER_08_PATH + "c08_varkhane_archive.tscn", "c08_namar_refuge_port": CHAPTER_08_PATH + "c08_namar_refuge_port.tscn", "c08_namar_ledger_vault": CHAPTER_08_PATH + "c08_namar_ledger_vault.tscn", "c08_azravel_shelter_temple": CHAPTER_08_PATH + "c08_azravel_shelter_temple.tscn", "c08_azravel_dogma_court": CHAPTER_08_PATH + "c08_azravel_dogma_court.tscn", "c08_korem_fortified_academy": CHAPTER_08_PATH + "c08_korem_fortified_academy.tscn", "c08_korem_protocol_archive": CHAPTER_08_PATH + "c08_korem_protocol_archive.tscn",
+    "c09_tree_node": CHAPTER_09_PATH + "c09_tree_node.tscn", "c09_seven_lenses": CHAPTER_09_PATH + "c09_seven_lenses.tscn", "c09_living_resonance": CHAPTER_09_PATH + "c09_living_resonance.tscn", "c09_saan_network": CHAPTER_09_PATH + "c09_saan_network.tscn", "c09_fear_basin": CHAPTER_09_PATH + "c09_fear_basin.tscn", "c09_deep_overlap": CHAPTER_09_PATH + "c09_deep_overlap.tscn", "c09_consensus_chamber": CHAPTER_09_PATH + "c09_consensus_chamber.tscn",
     "va01_threshold_gallery": DEEP_VESTIGE_PATH + "va01_threshold_gallery.tscn", "va02_singing_well": DEEP_VESTIGE_PATH + "va02_singing_well.tscn", "va03_hall_of_pairs": DEEP_VESTIGE_PATH + "va03_hall_of_pairs.tscn", "va04_silent_cloister": DEEP_VESTIGE_PATH + "va04_silent_cloister.tscn", "va05_memory_organ": DEEP_VESTIGE_PATH + "va05_memory_organ.tscn", "va06_seventh_chamber": DEEP_VESTIGE_PATH + "va06_seventh_chamber.tscn",
     "vs01_gate_of_doctrine": DEEP_VESTIGE_PATH + "vs01_gate_of_doctrine.tscn", "vs02_countermeasure_hall": DEEP_VESTIGE_PATH + "vs02_countermeasure_hall.tscn", "vs03_black_arsenal": DEEP_VESTIGE_PATH + "vs03_black_arsenal.tscn", "vs04_civilian_foundry": DEEP_VESTIGE_PATH + "vs04_civilian_foundry.tscn", "vs05_last_strategy": DEEP_VESTIGE_PATH + "vs05_last_strategy.tscn", "vs06_war_table": DEEP_VESTIGE_PATH + "vs06_war_table.tscn",
     "vn01_outer_cloister": DEEP_VESTIGE_PATH + "vn01_outer_cloister.tscn", "vn02_silent_archive": DEEP_VESTIGE_PATH + "vn02_silent_archive.tscn", "vn03_sealed_dormitory": DEEP_VESTIGE_PATH + "vn03_sealed_dormitory.tscn", "vn04_watchers_well": DEEP_VESTIGE_PATH + "vn04_watchers_well.tscn", "vn05_last_vigil": DEEP_VESTIGE_PATH + "vn05_last_vigil.tscn", "vn06_seal_heart": DEEP_VESTIGE_PATH + "vn06_seal_heart.tscn"
@@ -62,6 +64,7 @@ func start_chapter_05() -> bool: return CampaignState.current_chapter_id == "cha
 func start_chapter_06() -> bool: return CampaignState.current_chapter_id == "chapter_06_absent" and _start_exp("c06_timeless_garden")
 func start_chapter_07() -> bool: return CampaignState.current_chapter_id == "chapter_07_living_responsible" and _start_exp("c07_engineer_refuge")
 func start_chapter_08() -> bool: return CampaignState.current_chapter_id == "chapter_08_outer_world" and _start_exp("c08_varkhane_border")
+func start_chapter_09() -> bool: return CampaignState.current_chapter_id == "chapter_09_veil_nature" and _start_exp("c09_tree_node")
 func start_deep_vestige(vestige_id: String) -> bool:
     if not DeepVestigeRuntime.is_unlocked(vestige_id): return false
     var entry_zone := DeepVestigeRuntime.entry_zone_for(vestige_id)
