58ebeb11e3b1e409d3ced4ccdeae7e131cd0f00b
hodaesu <aurelienfabre@ymail.com>
2026-08-18 12:53:44 +0200

    Reset campaign progress on new game

--- scripts/core/game_state.gd
+++ scripts/core/game_state.gd
@@ -24,6 +24,7 @@ func reset_new_game() -> void:
     EquipmentManager.reset_new_game()
     CreatureManager.reset_new_game()
     PoliticalState.reset_new_game()
+    CampaignState.reset_new_game()
     party = []
     for hero in DataLoader.heroes:
         var prepared_hero: Dictionary = hero.duplicate(true)
