585f1a6d9bbde92bd154e111d93501a1ef5269a7
hodaesu <aurelienfabre@ymail.com>
2026-08-19 07:30:50 +0200

    test: defer persistent UI journey runner attachment

--- scripts/core/ui_player_journey_bootstrap.gd
+++ scripts/core/ui_player_journey_bootstrap.gd
@@ -3,7 +3,10 @@ extends Node
 const RUNNER := preload("res://scripts/core/ui_player_journey_smoke_test.gd")
 
 func _ready() -> void:
-    var runner := RUNNER.new()
+    call_deferred("_start_runner")
+
+func _start_runner() -> void:
+    var runner: Node = RUNNER.new()
     runner.name = "UIPlayerJourneySmokeRunner"
     get_tree().root.add_child(runner)
     runner.call_deferred("run")
