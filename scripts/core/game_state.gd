acb14bb03eee9e33f9437b173f5e84fae8a1ff35
hodaesu <aurelienfabre@ymail.com>
2026-08-23 18:05:38 +0200

    feat: reset side quests on new game

--- scripts/core/game_state.gd
+++ scripts/core/game_state.gd
@@ -29,6 +29,7 @@ func reset_new_game() -> void:
     CreatureManager.reset_new_game()
     PoliticalState.reset_new_game()
     CampaignState.reset_new_game()
+    SideQuestRuntime.reset_new_game()
     BountyContractDirector.reset_new_game()
     AshlandsRuntime.reset_world_progression()
     Chapter01Runtime.reset_new_game()
