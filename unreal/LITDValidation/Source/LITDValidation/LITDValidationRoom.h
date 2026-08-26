fe0ba09652058c536d104418c8d579dfb6018726
hodaesu <aurelienfabre@ymail.com>
2026-08-26 11:50:48 +0200

    feat(unreal): define procedural validation room

---
+++ unreal/LITDValidation/Source/LITDValidation/LITDValidationRoom.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "LITDValidationRoom.generated.h"
+
+UCLASS()
+class LITDVALIDATION_API ALITDValidationRoom : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    ALITDValidationRoom();
+    virtual void OnConstruction(const FTransform& Transform) override;
+
+private:
+    UPROPERTY()
+    TArray<TObjectPtr<UStaticMeshComponent>> Geometry;
+
+    void Rebuild();
+    UStaticMeshComponent* AddBlock(const FName Name, const FVector& Location, const FVector& Scale, const FLinearColor& Color);
+};
