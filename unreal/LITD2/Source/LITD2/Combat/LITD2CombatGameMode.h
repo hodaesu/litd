ec46d4c190886f02fb22caff802e5e013c992018
hodaesu <aurelienfabre@ymail.com>
2026-09-01 16:43:02 +0200

    litd2: add combat vertical slice game mode

---
+++ unreal/LITD2/Source/LITD2/Combat/LITD2CombatGameMode.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LITD2CombatGameMode.generated.h"
+
+UCLASS(Blueprintable)
+class LITD2_API ALITD2CombatGameMode : public AGameModeBase
+{
+    GENERATED_BODY()
+
+public:
+    ALITD2CombatGameMode();
+};
