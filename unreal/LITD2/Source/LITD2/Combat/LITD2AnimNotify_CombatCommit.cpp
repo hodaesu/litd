6d9dd887a3dde76ea165465cdbcbb22bb091bae5
hodaesu <aurelienfabre@ymail.com>
2026-09-01 17:22:58 +0200

    litd2: dispatch exact combat frames from animation

---
+++ unreal/LITD2/Source/LITD2/Combat/LITD2AnimNotify_CombatCommit.cpp
@@ -0,0 +1,60 @@
+#include "Combat/LITD2AnimNotify_CombatCommit.h"
+
+#include "Combat/LITD2AshWandererCharacter.h"
+#include "Combat/LITD2LineBreakerCharacter.h"
+#include "Combat/LITD2PlayerCombatCharacter.h"
+#include "Combat/LITD2SareiCrossbowCharacter.h"
+#include "Components/SkeletalMeshComponent.h"
+
+void ULITD2AnimNotify_CombatCommit::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+    const FAnimNotifyEventReference& EventReference)
+{
+    Super::Notify(MeshComp, Animation, EventReference);
+    if (!MeshComp) return;
+
+    AActor* Owner = MeshComp->GetOwner();
+    if (!Owner) return;
+
+    switch (CommitEvent)
+    {
+        case ELITD2CombatCommitEvent::PlayerQueuedAttack:
+            if (ALITD2PlayerCombatCharacter* Player = Cast<ALITD2PlayerCombatCharacter>(Owner))
+            {
+                Player->CommitQueuedAttackFromAnimation();
+            }
+            break;
+
+        case ELITD2CombatCommitEvent::AshWandererAttack:
+            if (ALITD2AshWandererCharacter* Wanderer = Cast<ALITD2AshWandererCharacter>(Owner))
+            {
+                Wanderer->CommitAttackFromAnimation();
+            }
+            break;
+
+        case ELITD2CombatCommitEvent::LineBreakerSevereAttack:
+            if (ALITD2LineBreakerCharacter* Breaker = Cast<ALITD2LineBreakerCharacter>(Owner))
+            {
+                Breaker->CommitSevereAttackFromAnimation();
+            }
+            break;
+
+        case ELITD2CombatCommitEvent::SareiCrossbowRelease:
+            if (ALITD2SareiCrossbowCharacter* Crossbow = Cast<ALITD2SareiCrossbowCharacter>(Owner))
+            {
+                Crossbow->ReleaseShotFromAnimation();
+            }
+            break;
+    }
+}
+
+FString ULITD2AnimNotify_CombatCommit::GetNotifyName_Implementation() const
+{
+    switch (CommitEvent)
+    {
+        case ELITD2CombatCommitEvent::PlayerQueuedAttack: return TEXT("LITD2 Player Hit");
+        case ELITD2CombatCommitEvent::AshWandererAttack: return TEXT("LITD2 Ash Wanderer Hit");
+        case ELITD2CombatCommitEvent::LineBreakerSevereAttack: return TEXT("LITD2 Line Breaker Impact");
+        case ELITD2CombatCommitEvent::SareiCrossbowRelease: return TEXT("LITD2 Crossbow Release");
+    }
+    return TEXT("LITD2 Combat Commit");
+}
