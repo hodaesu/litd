6216f74ef435bed7a2dd64764772515675f14693
hodaesu <aurelienfabre@ymail.com>
2026-09-03 06:37:10 +0200

    chore(veilleurs): restore full SkillData fields in skeleton

--- litd_universe/les_veilleurs/preproduction/gd_skeletons/ability_definition.gd
+++ litd_universe/les_veilleurs/preproduction/gd_skeletons/ability_definition.gd
@@ -8,6 +8,8 @@ extends Resource
 @export var owner_scope: StringName
 @export var tree_id: StringName
 @export_range(1, 15, 1) var slot_index: int = 1
+@export var rank: StringName
+@export var tier: StringName
 @export var display_name_key: StringName
 @export var description_key: StringName
 
@@ -15,12 +17,19 @@ extends Resource
 @export var targeting_mode: StringName
 @export var range_profile: StringName
 
+@export var endurance_cost: StringName
+@export var maintenance_cost: StringName
+@export var material_cost: StringName
+@export var preparation_profile: StringName
+@export var recovery_profile: StringName
+
 @export var required_body_functions: Array[StringName] = []
 @export var forbidden_body_states: Array[StringName] = []
 @export var impact_types: Array[StringName] = []
 
 @export var physical_power: StringName
 @export var precision: StringName
+@export var user_risk: StringName
 @export var allowed_zones: Array[StringName] = []
 @export var preferred_zones: Array[StringName] = []
 @export var lesion_rule_ids: Array[StringName] = []
@@ -34,7 +43,6 @@ extends Resource
 @export var vibration_profile: StringName
 @export var biological_profile: StringName
 
-@export var user_risk: StringName
 @export var friendly_fire: bool = false
 @export var prerequisite_ids: Array[StringName] = []
 @export var synergy_tags: Array[StringName] = []
@@ -55,6 +63,10 @@ func validate_contract() -> PackedStringArray:
         errors.append("action_type is required")
     if targeting_mode.is_empty():
         errors.append("targeting_mode is required")
+    if preparation_profile.is_empty():
+        errors.append("preparation_profile must be explicit")
+    if recovery_profile.is_empty():
+        errors.append("recovery_profile must be explicit")
     if animation_intent.is_empty():
         errors.append("animation_intent is required")
     return errors
