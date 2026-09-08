69f82da3b7a389cdeff71a905aac426123001999
hodaesu <aurelienfabre@ymail.com>
2026-09-08 23:35:59 +0200

    test(knowledge): add resolver smoke bootstrap

---
+++ scripts/core/discovery_resolver_goule_smoke_bootstrap.gd
@@ -0,0 +1,8 @@
+extends Node
+const RUNNER := preload("res://scripts/core/discovery_resolver_goule_smoke_test.gd")
+func _ready() -> void:
+    call_deferred("_run")
+func _run() -> void:
+    var runner = RUNNER.new()
+    get_tree().root.add_child(runner)
+    runner.call_deferred("run")
